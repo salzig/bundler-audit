25c83cffe9b76402592a624dc7695a69983b7bdc
gnomus <gnomus@gnomus.de>
2019-04-09 18:21:18 +0200

    escape some characters for valid xml

--- lib/bundler/audit/presenter/junit.rb
+++ lib/bundler/audit/presenter/junit.rb
@@ -12,9 +12,9 @@ module Bundler
 
         def advisory_ref(advisory)
           if advisory.cve
-            "CVE-#{advisory.cve}"
+            xml_escape "CVE-#{advisory.cve}"
           elsif advisory.osvdb
-            advisory.osvdb
+            xml_escape advisory.osvdb
           end
         end
 
@@ -29,14 +29,25 @@ module Bundler
 
         def advisory_solution(advisory)
           unless advisory.patched_versions.empty?
-            "upgrade to #{advisory.patched_versions.join(', ')}"
+            xml_escape "upgrade to #{advisory.patched_versions.join(', ')}"
           else
             "remove or disable this gem until a patch is available!"
           end
         end
 
         def bundle_title(bundle)
-          "#{advisory_criticality(bundle.advisory).upcase} #{bundle.gem.name}(#{bundle.gem.version}) #{bundle.advisory.title}"
+          xml_escape "#{advisory_criticality(bundle.advisory).upcase} #{bundle.gem.name}(#{bundle.gem.version}) #{bundle.advisory.title}"
+        end
+
+        def xml_escape(string)
+          string.gsub(
+            /[<>"'&]/,
+            '<' => '&lt;',
+            '>' => '&gt;',
+            '"' => '&quot;',
+            '\'' => '&apos;',
+            '&' => '&amp;',
+          )
         end
 
         def template_string
@@ -45,14 +56,14 @@ module Bundler
 <testsuites id="<%= Time.now.to_i %>" name="Bundle Audit" tests="225" failures="1262">
   <testsuite id="Gemfile" name="Ruby Gemfile" failures="<%= @advisory_bundles.size %>">
     <%- @advisory_bundles.each do |bundle| -%>
-    <testcase id="<%= bundle.gem.name %>" name="<%= bundle_title(bundle) %>">
-      <failure message="<%= bundle.advisory.title %>" type="<%= bundle.advisory.criticality %>">
-Name: <%= bundle.gem.name %>
-Version: <%= bundle.gem.version %>
+    <testcase id="<%= xml_escape(bundle.gem.name) %>" name="<%= bundle_title(bundle) %>">
+      <failure message="<%= xml_escape(bundle.advisory.title) %>" type="<%= xml_escape(bundle.advisory.criticality) %>">
+Name: <%= xml_escape(bundle.gem.name) %>
+Version: <%= xml_escape(bundle.gem.version) %>
 Advisory: <%= advisory_ref(bundle.advisory) %>
 Criticality: <%= advisory_criticality(bundle.advisory) %>
-URL: <%= bundle.advisory.url %>
-Title: <%= bundle.advisory.title %>
+URL: <%= xml_escape(bundle.advisory.url) %>
+Title: <%= xml_escape(bundle.advisory.title) %>
 Solution: <%= advisory_solution(bundle.advisory) %>
       </failure>
     </testcase>
